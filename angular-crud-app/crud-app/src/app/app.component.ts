686d319674d1df02ed73749a57daf3a8aa5c8150
sunitharavuri <sravuri150@gmail.com>
2018-01-29 10:59:31 -0800

    Add files via upload

---
+++ angular-crud-app/crud-app/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular 2 CRUD operation with an Array';
+  employees = [
+    {name:"hEY", position:"Programmer"},
+    {name:"around", position:"Developer"},
+    {name:"Mall", position:"ux De"}
+  ];
+  model:any={};
+  model2:any={};
+  msg:any="";
+  addEmployee(){
+    this.employees.push(this.model);
+    this.model = {};
+    this.msg = "Record is successfully added..... ";
+
+  }
+  deleteEmployee(i){
+    this.employees.splice(i,1);
+    this.msg = "Record is successfully deleted..... ";
+
+  }
+  myValue;
+  editEmployee(k){
+    this.model2.name = this.employees[k].name;
+    this.model2.position = this.employees[k].position;
+    this.myValue = k;
+
+  }
+  updateEmployee(){
+    let k= this.myValue;
+    for(let i=0; i<this.employees.length;i++){
+      if(i==k){
+        this.employees[i]= this.model2;
+        this.model2 = {};
+        this.msg = "Record is successfully updated..... ";
+      }
+
+    }
+
+  }
+  clickMe(){
+    this.msg = "";
+  }
+}
+localStorage.setItem('myCat', 'Tom');
